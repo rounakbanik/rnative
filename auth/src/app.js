eefed4d2934aad5d6e9d41bda883a1a53b712a93
rounakbanik <rounakbanik@gmail.com>
2017-03-31 00:59:29 +0530

    Add auth.

---
+++ auth/src/app.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header, Button, Spinner } from './components/common';
+import LoginForm from './components/LoginForm';
+
+class App extends Component {
+  state = { loggedIn: null };
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyBkQeYxIyVZH0bZuYEWOuHq6Nmfa1LoBbM",
+      authDomain: "auth-3a42a.firebaseapp.com",
+      databaseURL: "https://auth-3a42a.firebaseio.com",
+      storageBucket: "auth-3a42a.appspot.com",
+      messagingSenderId: "156190316767"
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Button onPress={() => firebase.auth().signOut()}>Log Out</Button>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
+  }
+
+  render() {
+    return (
+      <View>
+        <Header headerText="Authentication" />
+        {this.renderContent()}
+      </View>
+    );
+  }
+}
+
+export default App;
